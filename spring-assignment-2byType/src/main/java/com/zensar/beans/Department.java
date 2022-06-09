b6ccee72173125199d1f535ec5498e67dfa05054
PavanButke <75151054+PavanButke@users.noreply.github.com>
2022-06-09 15:58:01 +0530

    Assignment 2 byType

---
+++ spring-assignment-2byType/src/main/java/com/zensar/beans/Department.java
@@ -0,0 +1,35 @@
+package com.zensar.beans;
+
+public class Department {
+
+	
+	private int deptId;
+	private String deptName;
+	public Department() {
+		super();
+	}
+	public Department(int deptId, String deptName) {
+		super();
+		this.deptId = deptId;
+		this.deptName = deptName;
+	}
+	public int getDeptId() {
+		return deptId;
+	}
+	public void setDeptId(int deptId) {
+		this.deptId = deptId;
+	}
+	public String getDeptName() {
+		return deptName;
+	}
+	public void setDeptName(String deptName) {
+		this.deptName = deptName;
+	}
+	@Override
+	public String toString() {
+		return "Departement [deptId=" + deptId + ", deptName=" + deptName + "]";
+	}
+
+	
+	
+}
